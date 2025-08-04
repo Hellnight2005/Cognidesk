1447d382144e2911bdc5002de2a432adec940ed4
Abhijeet <abhijeet2005shinde@gmail.com>
2025-08-04 22:11:35 +0530

    commit

--- qdrant-idea-service/search/searchQdrant.js
+++
@@ -1,9 +0,0 @@
-const { searchFromQdrant, ensureCollection } = require("../vectorDb");
-
-async function searchQdrant(vector, topK = 5) {
-  await ensureCollection();
-  const results = await searchFromQdrant(vector, topK);
-  return results;
-}
-
-module.exports = searchQdrant;
